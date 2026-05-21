00fe03a00c602778da27f1c72be160f45ae37a4c
mystricman0-cell <mystricman0@gmail.com>
2026-05-21 16:48:02 +0530

    Update Youtube.py

--- NOBITA_MUSIC/platforms/Youtube.py
+++ NOBITA_MUSIC/platforms/Youtube.py
@@ -8,8 +8,8 @@ from pyrogram.enums import MessageEntityType
 from pyrogram.types import Message
 from youtubesearchpython.__future__ import VideosSearch
 
-from NOBITA_MUSIC.utils.database import is_on_off
-from NOBITA_MUSIC.utils.formatters import time_to_seconds
+from RONALDO_MUSIC.utils.database import is_on_off
+from ROMALDO_MUSIC.utils.formatters import time_to_seconds
 
 async def shell_cmd(cmd):
     proc = await asyncio.create_subprocess_shell(
