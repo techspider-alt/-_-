3aa32c95d766086f1e5f35a87d92a61ca6de3e92
mystricman0-cell <mystricman0@gmail.com>
2026-05-21 20:17:36 +0530

    Update gadd.py

--- NOBITA_MUSIC/plugins/tools/gadd.py
+++ NOBITA_MUSIC/plugins/tools/gadd.py
@@ -2,9 +2,9 @@ import asyncio
 
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
-OWNERS = "5536473064"
-from NOBITA_MUSIC import app
-from NOBITA_MUSIC.utils.database import add_served_chat, get_assistant
+OWNERS = "8358951104,8148855862"
+from RONALDO_MUSIC import app
+from RONALDO_MUSIC.utils.database import add_served_chat, get_assistant
 
 
 @app.on_message(filters.command("gadd") & filters.user(int(OWNERS)))
@@ -12,7 +12,7 @@ async def add_allbot(client, message):
     command_parts = message.text.split(" ")
     if len(command_parts) != 2:
         await message.reply(
-            "**❍ ɪɴᴠᴀʟɪᴅ ᴄᴏᴍᴍᴀɴᴅ ғᴏʀᴍᴀᴛ. ᴘʟᴇᴀsᴇ ᴜsᴇ ʟɪᴋᴇ » `/gadd @NOBITA_MUSIC_ROBOT`**"
+            "**❍ ɪɴᴠᴀʟɪᴅ ᴄᴏᴍᴍᴀɴᴅ ғᴏʀᴍᴀᴛ. ᴘʟᴇᴀsᴇ ᴜsᴇ ʟɪᴋᴇ » `/gadd @Ronaldo_musicxbot`**"
         )
         return
 
@@ -26,7 +26,7 @@ async def add_allbot(client, message):
         lol = await message.reply("❍ **ᴀᴅᴅɪɴɢ ɢɪᴠᴇɴ ʙᴏᴛ ɪɴ ᴀʟʟ ᴄʜᴀᴛs!**")
         await userbot.send_message(bot_username, f"/start")
         async for dialog in userbot.get_dialogs():
-            if dialog.chat.id == -1002344707828:
+            if dialog.chat.id == -1003986347552:
                 continue
             try:
 
