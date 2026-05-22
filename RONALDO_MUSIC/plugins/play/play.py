2368a80bd5dbd0c06427716aad69010258e6cbc7
mystricman0-cell <mystricman0@gmail.com>
2026-05-22 10:44:33 +0530

    fix: play.py - replace undefined vars, remove stream_type kwarg

--- RONALDO_MUSIC/plugins/play/play.py
+++ RONALDO_MUSIC/plugins/play/play.py
@@ -292,7 +292,7 @@ async def play_commnd(
             return await mystic.delete()
         else:
             try:
-                await RAUSHAN.stream_call(url)
+                await NOBITA.stream_call(url)
             except NoActiveGroupCall:
                 await mystic.edit_text(_["black_9"])
                 return await app.send_message(
